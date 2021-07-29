819704a97bb57bbc09140583522738465784f969
Matheus Tagomori <mathschmidtt@gmail.com>
2021-07-29 11:11:59 -0300

    added tp1_signal practice

---
+++ practice-1/tp1_receiver.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include<signal.h>
+
+// Signal handler functions
+void usr1_handler(int num) {
+    write(STDOUT_FILENO, "Signal USR1 Received\n", 22);
+}
+void usr2_handler(int num) {
+    write(STDOUT_FILENO, "Signal USR2 Received\n", 22);
+}
+void term_handler(int num) {
+    write(STDOUT_FILENO, "Signal TERM Received\n", 22);
+}
+
+int main(int argc, char *argv[]) {
+
+    // Declaring signals to be waiting for and respective handler functions
+    signal(SIGUSR1, usr1_handler);
+    signal(SIGUSR2, usr2_handler);
+    signal(SIGTERM, term_handler);
+
+    // Transforming int pid to string pid (spid)
+    int pid = getpid();
+    char spid[10];
+    snprintf(spid, 10, "%d", pid);
+
+    // Transforming argv[1] to int 0 or 1 and saving it on wait variable
+    char *wait_str;
+    int wait;
+    long conv = strtol(argv[1], &wait_str, 10);
+    wait = conv;
+
+    // Busy Wait prints the PID
+    while(wait == 0) {
+        write(STDOUT_FILENO, "BUSY WAIT PID = ", 17);
+        write(STDOUT_FILENO, spid, 4);
+        write(STDOUT_FILENO, "\n", 1);
+        sleep(5);
+    }
+
+    // Blocking Wait prints the PID
+    while(wait == 1) {
+        write(STDOUT_FILENO, "BLOCKING WAIT PID = ", 21);
+        write(STDOUT_FILENO, spid, 4);
+        write(STDOUT_FILENO, "\n", 1);
+        pause();
+    }
+
+    return 0;
+}
---
+++ practice-1/tp1_signal.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include<signal.h>
+
+int check_process(pid_t pid) {
+
+    // Checa se o processo existe enviando um kill com signal 0
+    if (0 == kill(pid, 0)) {
+        printf("Process exists\n");
+        return(1);
+    } 
+    else {
+        printf("ERROR: Process does NOT exist\n");
+        return(0);
+    }
+}
+
+int main(int argc, char *argv[]){
+
+    char *p;
+    int pid;
+
+    long conv = strtol(argv[1], &p, 10);
+
+    pid = conv;
+
+    // Se o processo existe
+    if (check_process(pid)) {
+
+        if (strcmp ("SIGUSR1", argv[2]) == 0) {
+            kill(pid, SIGUSR1);
+        }
+
+        if (strcmp ("SIGUSR2", argv[2]) == 0) {
+            printf("SIGUSR1 SENT");
+            kill(pid, SIGUSR2);
+        }
+
+        if (strcmp ("SIGTERM", argv[2]) == 0) {
+            printf("SIGUSR1 SENT");
+            kill(pid, SIGINT);
+        }
+        
+    }
+
+    return 0;
+}
