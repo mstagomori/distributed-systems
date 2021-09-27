abc2697ab5108970aa39d733fdb0323ac334698a
CaioJrVS <caio.junior.vs@gmail.com>
2021-09-27 19:31:42 -0300

    add coordinator

--- practice-3/client/client.cpp
+++ practice-3/client/client.cpp
@@ -1,34 +1,36 @@
 #include <bits/stdc++.h>
-#include<stdlib.h>
-#include<sys/socket.h>
-#include<stdio.h>
-#include<iostream>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <wait.h>
 
 using namespace std;
 
-int atoi(const char* str)
+// retirado de: https://www.geeksforgeeks.org/write-your-own-atoi/
+int atoi(const char *str)
 {
     int sign = 1, base = 0, i = 0;
-     
+
     // if whitespaces then ignore.
     while (str[i] == ' ')
     {
         i++;
     }
-     
+
     // sign of number
     if (str[i] == '-' || str[i] == '+')
     {
         sign = 1 - 2 * (str[i++] == '-');
     }
-   
+
     // checking for valid input
     while (str[i] >= '0' && str[i] <= '9')
     {
         // handling overflow test case
-        if (base > INT_MAX / 10
-            || (base == INT_MAX / 10
-            && str[i] - '0' > 7))
+        if (base > INT_MAX / 10 || (base == INT_MAX / 10 && str[i] - '0' > 7))
         {
             if (sign == 1)
                 return INT_MAX;
@@ -40,13 +42,28 @@ int atoi(const char* str)
     return base * sign;
 }
 
-int main(int argc, char **argv){
-    int r,k,n;
-    r= atoi(argv[1]);
-    k= atoi(argv[2]);
-    n= atoi(argv[3]);
+int main(int argc, char **argv)
+{
+    int r, k, n, status;
+    r = atoi(argv[1]);
+    k = atoi(argv[2]);
+    n = atoi(argv[3]);
+    int pid[n];
 
-    cout<<argc<<"\n";
-    cout<<r<<'\n'<<k<<'\n'<<n<<endl;
-    cout<<*argv[1]<<'\n'<<argv[2]<<'\n'<<argv[3]<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        if((pid[i] = fork()) < 0 ){
+            printf("Failed to fork process %d", i);
+            exit(1);
+        }
+        if (pid[i] == 0)
+        {
+        }
+    }
+
+    while (n > 0)
+    {
+        wait(&status);
+        n--;
+    }
 }
